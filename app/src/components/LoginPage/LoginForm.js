9621b8179214b33193c69d673828fc7af035e7be
nmarasigan16 <egarsenal16@gmail.com>
2017-11-14 13:07:58 -0600

    why idea

--- app/src/components/LoginPage/LoginForm.js
+++ app/src/components/LoginPage/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     View,
     Dimensions,
@@ -12,46 +12,84 @@ import TextComponent from '../general/text/TextComponent';
 import FacebookLogin from './FacebookLogin';
 
 
-const LoginForm = (props) => {
-    const {width, height} = Dimensions.get('window');
+class LoginForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+        this.onLogin = this.onLogin.bind(this);
+    }
 
-    return (
-        <View style={{alignSelf: 'center', flexDirection: 'column', width: 3*width/6, alignContent: 'center', opacity: props.opacity, marginTop: height/30}}>
-            <FacebookLogin/>
-            <View style={{flexDirection: 'row', marginTop: height/50}}>
-                <Divider style={{width: width/6, alignSelf: 'center'}} />
-                <View style={{width: width/6, alignSelf: 'center'}}>
-                    <TextComponent
-                        style={{color: colorScheme.accent, textAlign: 'center'}}>
-                        or
-                    </TextComponent>
+    onLogin(e) {
+        console.log(this.state.username);
+    }
+
+    render() {
+        const { width, height } = Dimensions.get('window');
+        const { username, password } = this.state;
+
+        return (
+            <View style={{
+                alignSelf: 'center',
+                flexDirection: 'column',
+                width: 3 * width / 6,
+                alignContent: 'center',
+                opacity: this.props.opacity,
+                marginTop: height / 30
+            }}>
+                <FacebookLogin/>
+                <View style={{flexDirection: 'row', marginTop: height / 50}}>
+                    <Divider style={{width: width / 6, alignSelf: 'center'}}/>
+                    <View style={{width: width / 6, alignSelf: 'center'}}>
+                        <TextComponent
+                            style={{color: colorScheme.accent, textAlign: 'center'}}>
+                            or
+                        </TextComponent>
+                    </View>
+                    <Divider style={{width: width / 6, alignSelf: 'center'}}/>
                 </View>
-                <Divider style={{width: width/6, alignSelf:'center'}}/>
+                <TextInput
+                    color={'silver'}
+                    label={'Username'}
+                    labelColor={'silver'}
+                    value={username}
+                    onChangeText={(username) => this.setState({username})}
+                />
+                <TextInput
+                    color={'silver'}
+                    label={'Password'}
+                    labelColor={'silver'}
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({password})}
+                    value={password}
+                />
+                <TouchableOpacity onPress={this.onLogin}>
+                    <TextComponent
+                        style={{
+                            textAlign: 'center',
+                            color: colorScheme.accent,
+                            fontSize: width / 20,
+                            marginTop: height / 30
+                        }}>
+                        login
+                    </TextComponent>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <TextComponent
+                        style={{
+                            textAlign: 'center',
+                            color: colorScheme.accent,
+                            fontSize: width / 30,
+                            marginTop: height / 50
+                        }}>
+                        new here? sign up!
+                    </TextComponent>
+                </TouchableOpacity>
             </View>
-            <TextInput
-                color={'silver'}
-                label={'Username'}
-                labelColor={'silver'}
-            />
-            <TextInput
-                color={'silver'}
-                label={'Password'}
-                labelColor={'silver'}
-                secureTextEntry={true}
-            />
-            <TouchableOpacity>
-                <TextComponent
-                    style={{
-                        textAlign: 'center',
-                        color: colorScheme.accent,
-                        fontSize: width/20,
-                        margin: height/30
-                    }}>
-                    login
-                </TextComponent>
-            </TouchableOpacity>
-        </View>
-    )
-};
+        )
+    }
+}
 
 export default LoginForm;
--- docs/source/functionality/index.rst
+++ docs/source/functionality/index.rst
@@ -1,17 +1,43 @@
 BarCode Functionality
 =====================
 
-This describes the current state of functionality of the application.  WIP indicates that the functionality is a work in progress.
+This describes the current state of functionality of the application.  |WIP| indicates that the functionality is a work in progress.
 
 Application Front-End
 ---------------------
 
-+ Login Screen |checkmark|
-    
++ Login Screen
+
+  - Styled |check|
+  - Linked with BarCode API |WIP|
+  - Linked with Facebook API |WIP|
+
++ Specials Screen
+
+  + Specials Page
+
+    + Styled |check|
+
+  + Specials Row
+
+    + Styled |WIP|
+
+  + Linked with BarCode API |x|
+
++ Bar Profile
+
+  + Styled |WIP|
+  + Linked with BarCode API |x|
+  + Management Tools
+
+    + Update cover |x|
+    + Create Specials |x|
 
 API
 ---
 
 + Login Endpoint
   
-.. |checkmark| unicode:: U+2713
+.. |check| unicode:: U+2713
+.. |WIP| unicode:: U+1F680
+.. |x| unicode:: U+2717
