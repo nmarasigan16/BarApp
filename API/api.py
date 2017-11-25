2ac80a0a550767a8c35192835e471a7918784d9b
achhugani23 <aachhugani@gmail.com>
2017-11-25 14:24:59 -0500

    added bars and user status

--- API/api.py
+++ API/api.py
@@ -2,45 +2,21 @@
 #used the following tutorial https://realpython.com/blog/python/token-based-authentication-with-flask/#register-route
 import flask
 import datetime
+import sys, traceback
 
 from user import User
+from bar import Bar
 from flask import Flask, jsonify, request, make_response
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
+from db_ops import DBOperations
 
 
 __name__ = 'BarApp'
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 mongo = PyMongo(app)
-
-def add_to_db(user, collection):
-    """
-    Adds a user to the database
-    :return: Nothing
-    """
-    obj = {'username': user.username, 'password':user.password,
-        'age': user.age, 'gender': user.gender}
-    collection.insert_one(obj)
-
-def blacklist_token(token):
-    """
-    Blacklists a token so it cannot be used in the future
-    :return: Nothing
-    """
-    obj = {'token':token, 'blacklisted_on': datetime.datetime.now()}
-    mongo.db.BlackList.insert_one(obj)
-
-def check_if_blacklisted(token):
-    """
-    Checks if a token has been blacklisted
-    :return: Boolean, True if blacklisted, False if not
-    """
-    token = mongo.db.BlackList.find_one({'token':token})
-    if token is None:
-        return False
-    else:
-        return True
+db_ops = DBOperations(mongo)
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -49,24 +25,32 @@ def register():
     :return: String and status
     """
     content = request.get_json()
-    collection = mongo.db.Users
-    user = collection.find_one({'username': content['username']})
+    user = db_ops.check_bar_user_db(content['username'])
     if user is None:
         try:
-            user = User(content['username'], content['password'], content['age'], content['gender'], bcrypt)
+            if content['bar']:
+                user = Bar(content['username'], content['password'], content['name'],
+                    content['location'], content['phone'], bcrypt)
+            else:
+                user = User(content['username'], content['password'], content['name'],
+                    content['age'], content['gender'], bcrypt)
             auth_token = user.encode_auth_token(user.username)
             responseObject = {
                 'status': 'success',
                 'message': 'Successfully registered.',
                 'auth_token': auth_token.decode()
             }
-            add_to_db(user, collection)
+            if content['bar']:
+                db_ops.add_bar_to_db(user)
+            else:
+                db_ops.add_user_to_db(user)
             return make_response(jsonify(responseObject)), 201
         except Exception as e:
             responseObject = {
                 'status': 'fail',
                 'message': 'Some error occurred. Please try again.'
             }
+            traceback.print_exc(file=sys.stdout)
             return make_response(jsonify(responseObject)), 401
     else:
         responseObject = {
@@ -82,12 +66,11 @@ def login():
     :return: String and status
     """
     content = request.get_json()
-    collection = mongo.db.Users
     try:
-        user = collection.find_one({'username': content['username']})
+        user = db_ops.check_bar_user_db(content['username'])
         if user and bcrypt.check_password_hash(
-            user['password'], content['password']):
-            auth_token = User.encode_auth_token(user['username'])
+            user[1]['password'], content['password']):
+            auth_token = User.encode_auth_token(user[1]['username'])
             if auth_token:
                 responseObject = {
                     'status': 'success',
@@ -115,21 +98,19 @@ def logout():
     :return: String and status
     """
     auth_header = request.headers.get('Authorization')
-    print(auth_header)
     if auth_header:
         auth_token = auth_header
     else:
         auth_token = ''
-    if auth_token and not check_if_blacklisted(auth_token):
+    if auth_token and not db_ops.check_if_blacklisted(auth_token):
         resp = User.decode_auth_token(auth_token)
-        print (type(resp))
         if isinstance(resp, str):
             try:
                 responseObject = {
                     'status': 'success',
                     'message': 'Successfully logged out.'
                 }
-                blacklist_token(auth_token)
+                db_ops.blacklist_token(auth_token)
                 return  (jsonify(responseObject)), 200
             except Exception as e:
                 responseObject = {
@@ -150,4 +131,58 @@ def logout():
         }
         return make_response(jsonify(responseObject)), 403
 
+@app.route('/status', methods=['GET'])
+def status():
+    """
+    Returns the status of the user, if they are logged in or not
+    :return: User object as a dictionary
+    """
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header
+    else:
+        auth_token = ''
+    if auth_token and not db_ops.check_if_blacklisted(auth_token):
+        resp = User.decode_auth_token(auth_token)
+        if isinstance(resp, str):
+            user = db_ops.check_bar_user_db(resp)
+            try:
+                if user[0] == 'User':
+                    responseObject = {
+                        'status': 'success',
+                        'username': user[1]['username'],
+                        'bar': False
+                    }
+                    return make_response(jsonify(responseObject)), 200
+                elif user[0] == 'Bar':
+                    responseObject = {
+                        'status': 'success',
+                        'username': user[1]['username'],
+                        'bar': True
+                    }
+                    return make_response(jsonify(responseObject)), 200
+                else:
+                    responseObject = {
+                    'status': 'fail',
+                    'message': 'User does not exist'
+                    }
+                    return make_response(jsonify(responseObject)), 401
+            except Exception as e:
+                responseObject = {
+                    'status': 'fail',
+                    'message': e
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'Provide a valid auth token.'
+        }
+        return make_response(jsonify(responseObject)), 403
 # app.run(debug=True)
